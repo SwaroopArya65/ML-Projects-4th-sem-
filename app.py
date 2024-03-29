3300cd6eba85ce5d975ac16f251f47f798b4b0e3
iamsurajx <surajc8800@gmail.com>
2024-03-29 11:59:26 +0530

    major change in style

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
