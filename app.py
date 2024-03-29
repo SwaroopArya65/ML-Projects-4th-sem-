f75be8c5f5d68ccde294473618b000113e9153c3
Arya Swaroop <aryaswaroop027@gmail.com>
2024-03-29 17:10:26 +0530

    Modify in contact us form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,17 +14,26 @@ def about():
 def contact():
     return render_template("contact.html")
 
+
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
+
+# @app.route('/dashboard')
+# def dashboard():
+#     return render_template("dashboard.html")
 
 @app.route('/heart')
 def heart():
     return render_template("heart.html")
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
+# @app.route('/contact')
+# def contact():
+#     return render_template("contact.html")
+
+@app.route('/parkinsons')
+def parkinsons():
+    return render_template("parkinsons.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
